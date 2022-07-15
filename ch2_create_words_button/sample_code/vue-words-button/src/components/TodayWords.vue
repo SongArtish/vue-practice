2ed5ae3f26a8729a4732cd72ec0983957f5d79e0
SongArtish <bulgen@naver.com>
2022-07-15 20:46:26 +0900

    ch1 ch2

---
+++ ch2_create_words_button/sample_code/vue-words-button/src/components/TodayWords.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>오늘의 말씀을 주시옵소서!</h1>
+    <button id="button" @click="pick">뽑기</button>
+    <h3>{{selectedWord}}</h3>
+    <h3 v-if="selectedWord.length">이렇게 살아가길 원합니다.</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodayWords',
+  data: function () {
+    return {
+      words: ['항상 기뻐하라', '쉬지말고 기도하라', '범사에 감사하라'],
+      selectedWord: ''
+    }
+  },
+  methods: {
+    pick: function () {
+      let idx = Math.floor(Math.random() * this.words.length)
+      this.selectedWord = this.words[idx]
+      // console.log(this.selectedWord)
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+#button {
+  background-color: #008CBA;
+  font-size: 5rem;
+  color: white;
+  border: solid #008CBA;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+#button:hover {
+  background-color: white;
+  color: #008CBA;
+}
+</style>
